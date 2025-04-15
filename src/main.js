aad775e3725542060dad0430a563308a3496b258
yourock1988 <113207809+yourock1988@users.noreply.github.com>
2025-04-15 23:19:36 +0300

    Include: materialize-css js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import './assets/css/main.css'
 
 import 'materialize-css/dist/css/materialize.css'
+import 'materialize-css/dist/js/materialize.js'
 
 import { createApp } from 'vue'
 import App from './App.vue'
