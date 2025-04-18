f4b4e0238bba9340209fe1dc6abe4900dcd1fbcf
tolik-85 <tolian8576@gmail.com>
2025-04-18 19:39:19 +0300

    add SwitchesCheckbox

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,18 @@
 import MButton from './components/MButton.vue'
 import MInputText from './components/MInputText.vue'
 import MFAB from './components/MFAB.vue'
+import SwitchesCheckbox from './components/SwitchesCheckbox.vue'
 
 export default {
-  components: { MButton, MInputText, MFAB },
+  components: { MButton, MInputText, MFAB, SwitchesCheckbox },
 
   data() {
     return {
       message: '',
       isShowFAB: false,
       hasPulseFAB: false,
+      checked: false,
+      isDisabled: false,
     }
   },
 }
@@ -37,6 +40,23 @@ export default {
 
     <article>
       <MFAB :isShow="isShowFAB" :hasPulse="hasPulseFAB" mui-icon="home" />
+    </article>
+
+    <article>
+      <MButton
+        :caption="checked ? 'ON' : 'OFF'"
+        @click="checked = !checked"
+        :is-toggle="!checked"
+      />
+      <MButton
+        :caption="isDisabled ? 'Enable' : 'Disable'"
+        @click="isDisabled = !isDisabled"
+        :is-toggle="isDisabled"
+      />
+    </article>
+
+    <article>
+      <SwitchesCheckbox v-model:checked="checked" :disabled="isDisabled" />
     </article>
   </section>
 </template>
---
+++ src/components/SwitchesCheckbox.vue
@@ -0,0 +1,44 @@
+<script>
+export default {
+  props: {
+    checked: {
+      type: Boolean,
+      default: false,
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  emits: ['update:checked'],
+  data() {
+    return {}
+  },
+}
+</script>
+<template>
+  <!-- Switch -->
+  <div class="switch">
+    <label>
+      Off
+      <input
+        :checked="checked"
+        type="checkbox"
+        @change="$emit('update:checked', $event.target.checked)"
+        :disabled="disabled"
+      />
+      <span class="lever"></span>
+      On
+    </label>
+  </div>
+
+  <!-- Disabled Switch -->
+  <!-- <div class="switch">
+    <label>
+      Off
+      <input disabled type="checkbox" />
+      <span class="lever"></span>
+      On
+    </label>
+  </div> -->
+</template>
